2e04531e1f6ec1d23ec72875ad980eee1796ab3c
Abishek R <abishek230606@gmail.com>
2026-05-20 18:00:39 +0530

    Board, Moves, Undo classes have been added.

---
+++ include/board/board.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <cstdint>
+#include <move.h>
+class board{
+    uint64_t board[12];
+};
---
+++ src/main.cpp
@@ -0,0 +1,4 @@
+#include <iostream>
+int main(){
+    std::cout<<"MY name is..."<<std::endl;
+}
